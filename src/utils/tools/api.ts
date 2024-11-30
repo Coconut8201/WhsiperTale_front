c8e156911983293bc792ae996c1f66b892b1021a
Coconut8201 <cocom714kv@gmail.com>
2024-12-01 03:41:48 +0800

    在128上設定ip

--- src/utils/tools/api.ts
+++ src/utils/tools/api.ts
@@ -1,49 +1,49 @@
 // back_port = 7943;
 
 export const enum apis {
-    //dev
-    startStory = `http://localhost:7943/story/startstory`,
-    GetStoryList = `http://localhost:7943/story/getstorylist_fdb`,
-    LLMGenStory = `http://localhost:7943/story/llm/genstory`,
-    GenImagePrompt = `http://localhost:7943/story/llm/genimageprompt`,
-    GetVoice = `http://localhost:7943/story/voice/take_voice`,
-    GetNewaudio = `http://163.13.202.120:9880`,
-    getAllSDModel = `http://163.13.202.120:7860/sdapi/v1/sd-models`,
-    uploadVoice = `http://localhost:7943/voice/uploadvoices`,
-    userLogin = "http://localhost:7943/user/login",
-    userLogout = "http://localhost:7943/user/logout",
-    userRegister = "http://localhost:7943/user/adduser",
-    getVoiceList = "http://localhost:7943/voiceset/getVoiceList",
-    verifyAuth = "http://localhost:7943/user/verify-auth",
+    // //dev
+    // startStory = `http://localhost:7943/story/startstory`,
+    // GetStoryList = `http://localhost:7943/story/getstorylist_fdb`,
+    // LLMGenStory = `http://localhost:7943/story/llm/genstory`,
+    // GenImagePrompt = `http://localhost:7943/story/llm/genimageprompt`,
+    // GetVoice = `http://localhost:7943/story/voice/take_voice`,
+    // GetNewaudio = `http://163.13.202.128:9880`,
+    // getAllSDModel = `http://163.13.202.128:7860/sdapi/v1/sd-models`,
+    // uploadVoice = `http://localhost:7943/voice/uploadvoices`,
+    // userLogin = "http://localhost:7943/user/login",
+    // userLogout = "http://localhost:7943/user/logout",
+    // userRegister = "http://localhost:7943/user/adduser",
+    // getVoiceList = "http://localhost:7943/voiceset/getVoiceList",
+    // verifyAuth = "http://localhost:7943/user/verify-auth",
 
 
 
     // //use
-    // startStory = `http://163.13.202.120:7943/story/startstory`,
-    // GetStoryList = `http://163.13.202.120:7943/story/getstorylist_fdb`,
-    // LLMGenStory = `http://163.13.202.120:7943/story/llm/genstory`,
-    // GenImagePrompt = `http://163.13.202.120:7943/story/llm/genimageprompt`,
-    // GetVoice = `http://163.13.202.120:7943/story/voice/take_voice`,
-    // GetNewaudio = `http://163.13.202.120:9880`,
-    // getAllSDModel = `http://163.13.202.120:7860/sdapi/v1/sd-models`,
-    // uploadVoice = `http://163.13.202.120:7943/voice/uploadvoices`,
+    // startStory = `http://163.13.202.128:7943/story/startstory`,
+    // GetStoryList = `http://163.13.202.128:7943/story/getstorylist_fdb`,
+    // LLMGenStory = `http://163.13.202.128:7943/story/llm/genstory`,
+    // GenImagePrompt = `http://163.13.202.128:7943/story/llm/genimageprompt`,
+    // GetVoice = `http://163.13.202.128:7943/story/voice/take_voice`,
+    // GetNewaudio = `http://163.13.202.128:9880`,
+    // getAllSDModel = `http://163.13.202.128:7860/sdapi/v1/sd-models`,
+    // uploadVoice = `http://163.13.202.128:7943/voice/uploadvoices`,
 
-    // userLogin = "http://163.13.202.120:7943/user/login",
-    // userRegister = "http://163.13.202.120:7943/user/adduser"
+    // userLogin = "http://163.13.202.128:7943/user/login",
+    // userRegister = "http://163.13.202.128:7943/user/adduser"
 
 
 
-    // // using in Nginx
-    // startStory = `https://163.13.202.120/api/story/startstory`,
-    // GetStoryList = `https://163.13.202.120/api/story/getstorylist_fdb`,
-    // LLMGenStory = `https://163.13.202.120/api/story/llm/genstory`,
-    // GenImagePrompt = `https://163.13.202.120/api/story/llm/genimageprompt`,
-    // GetVoice = `https://163.13.202.120/api/story/voice/take_voice`,
-    // GetNewaudio = `https://163.13.202.120/audio`,
-    // getAllSDModel = `https://163.13.202.120/sd/sdapi/v1/sd-models`,
-    // uploadVoice = `https://163.13.202.120/api/voice/uploadvoices`,
-    // userLogin = "https://163.13.202.120/api/user/login",
-    // userRegister = "https://163.13.202.120/api/user/adduser",
-    // getVoiceList = "https://163.13.202.120/api/voiceset/getVoiceList",
+    // using in Nginx
+    startStory = `https://163.13.202.128/api/story/startstory`,
+    GetStoryList = `https://163.13.202.128/api/story/getstorylist_fdb`,
+    LLMGenStory = `https://163.13.202.128/api/story/llm/genstory`,
+    GenImagePrompt = `https://163.13.202.128/api/story/llm/genimageprompt`,
+    GetVoice = `https://163.13.202.128/api/story/voice/take_voice`,
+    GetNewaudio = `https://163.13.202.128/audio`,
+    getAllSDModel = `https://163/sd/sdapi/v1/sd-models`,
+    uploadVoice = `https://163.13.202.128/api/voice/uploadvoices`,
+    userLogin = "https://163.13.202.128/api/user/login",
+    userRegister = "https://163.13.202.128/api/user/adduser",
+    getVoiceList = "https://163.13.202.128/api/voiceset/getVoiceList",
 
 }
