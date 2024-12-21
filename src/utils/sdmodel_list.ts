d3f60e56e0021b2a305b4a6a32a3315821c6e83b
Coconut8201 <cocom714kv@gmail.com>
2024-12-22 00:23:43 +0800

    刪除不符合期望的圖片模型、更新語音生成因內容太長而出錯的問題

--- src/utils/sdmodel_list.ts
+++ src/utils/sdmodel_list.ts
@@ -7,7 +7,6 @@ export interface sdmodel{
 export const sdmodel_list: sdmodel[] = [
     {sd_name: "pastelMixStylizedAnime_pastelMixPrunedFP16.safetensors [d01a68ae76]", show_name: "水彩風格", image_path: "/src/images/pastelMixStylizedAnime_pastelMixPrunedFP16.png"},
     {sd_name: "flux_dev.safetensors [4610115bb0]", show_name: "繪畫風格", image_path: "/src/images/flux_dev.png"},
-    {sd_name: "Pixel_Art_Diffusion_XL", show_name: "像素風格", image_path: "/src/images/Pixel_Art_Diffusion_XL.png"},
     {sd_name: "flux_dev.safetensors [4610115bb0]", show_name: "可愛卡通風格", image_path: "/src/images/picture_booksChildren_cartoon.jpeg"}, // <lora:children huiben11-20231007:1>
     { sd_name: 'rachelWalkerStyle_v1.ckpt [54ded95d83]', show_name: '水彩畫風格', image_path: '/src/images/rachelWalkerStyle_v1_image.jpeg' },
     { sd_name: 'splatterPunkXL_v10.safetensors [b5730102eb]', show_name: '賽博龐克風格', image_path: '/src/images/splatterPunkXL_v10_image.jpeg' },
